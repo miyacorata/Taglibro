2ff1c64dfdf02d817f0c65ab0e96b15c833c4eea
K Miyano <miyano@miyacorata.net>
2025-05-23 18:09:17 +0900

    ヘッダを取っ払った

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,10 @@
 @extends('layout')
 
 @section('main')
-    <section class="hero is-large is-light">
-        <div class="hero-body">
-            <p class="title mb-2">{{ config('app.name') }}</p>
-            <p class="subtitle">{{ config('app.description', 'Cognito-SP Sample App') }}</p>
-            @auth
-                <a href="{{ route('dashboard') }}" class="button">Dashboard</a>
-            @else
-                <a href="{{ route('login') }}" class="button">Login</a>
-            @endauth
+    <section class="hero is-info is-small">
+        <div class="hero-body px-6">
+            <p class="title mb-2"><a href="{{ url('/') }}" class="has-text-black">{{ config('app.name') }}</a></p>
+            <p class="subtitle is-6">{{ config('app.description', 'Cognito-SP Sample App') }}</p>
         </div>
     </section>
 @endsection
