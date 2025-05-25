356eef54641a8ad2f275198884757ca07d3568d6
K Miyano <miyano@miyacorata.net>
2025-05-26 08:13:12 +0900

    ユーザーテーブルを変更

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('sub')->unique();
+            $table->string('sub')->unique()->index();
+            $table->string('preferred_username')->unique();
             $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('icon_url')->nullable();
