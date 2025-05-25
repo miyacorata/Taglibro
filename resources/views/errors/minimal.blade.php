4318522338cec08284adddaae7bc460d30881775
K Miyano <miyano@miyacorata.net>
2025-05-26 02:33:02 +0900

    エラーメッセージの出し方を変えた

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,9 +1,9 @@
 @extends('layout')
 
 @section('main')
-    <section class="hero is-medium is-danger">
-        <div class="hero-body">
-            <p class="title mb-2">@yield('code') @yield('title', 'Error')</p>
+    <section class="hero is-medium">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-2 has-text-danger">@yield('code') @yield('title', 'Error')</p>
             <p class="subtitle">@yield('message')</p>
             <a href="{{ url('/') }}" class="button">Return to Home</a>
         </div>
