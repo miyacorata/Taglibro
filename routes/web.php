69aa29496cdbe2a495425d19be69c7b82bc9d77a
K Miyano <miyano@miyacorata.net>
2025-05-28 23:58:24 +0900

    管理画面系のルーティングをミドルウェアでまとめた

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserDataController;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
@@ -12,10 +13,7 @@ Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->nam
 Route::get('/callback', [App\Http\Controllers\AuthController::class, 'callback']);
 Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', [\App\Http\Controllers\UserDataController::class, 'index'])
-    ->middleware(Authenticate::class.':cognito')->name('dashboard');
-Route::post('/user/update', [\App\Http\Controllers\UserDataController::class, 'update'])->name('user.update');
-
-Route::get('/test', function () {
-    return dump(Auth::user()->getAuthIdentifier(), Session::get('user'));
+Route::middleware(Authenticate::class.':cognito')->group(function () {
+    Route::get('/dashboard', [UserDataController::class, 'index'])->name('dashboard');
+    Route::post('/user/update', [UserDataController::class, 'update'])->name('user.update');
 });
