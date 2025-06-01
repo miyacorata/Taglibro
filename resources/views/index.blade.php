024e72321064a5fd981fa33d58be99ce1f7269b6
K Miyano <miyano@miyacorata.net>
2025-06-02 01:45:59 +0900

    表記揺れ修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <section class="section">
             <h1 class="title">最近の記事</h1>
-            <div class="subtitle is-6">Lastatempaj Afiŝoj</div>
+            <div class="subtitle is-6">Lastatempaj Artikoloj</div>
         </section>
     </div>
 @endsection
