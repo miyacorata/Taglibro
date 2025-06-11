0865e68340354f75ac1b0fe17ddb00068ddb7163
K Miyano <miyano@miyacorata.net>
2025-06-11 18:00:23 +0900

    /admin に対しダッシュボードへリダイレクトするルールを追加

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/callback', [App\Http\Controllers\AuthController::class, 'callback']
 Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 
 Route::prefix('admin')->middleware(Authenticate::class.':cognito')->group(function () {
+    Route::redirect('/', '/admin/dashboard');
     Route::get('/dashboard', [UserDataController::class, 'index'])->name('dashboard');
     Route::post('/user/update', [UserDataController::class, 'update'])->name('user.update');
     Route::resource('/article', ArticleDataController::class)->names('article');
